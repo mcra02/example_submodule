451e3946e1dc80c9c5f9a9c5336630249e2b1e7c
Maicol C Rodrigo <maicolcrodrigoa@gmail.com>
2022-03-19 17:48:43 -0500

    prueba commit submodulo index.ts

---
+++ src/i18n2/index.ts
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+// export { default as en } from './en/en.json'
+// // export {default as es_es} from './es_es/es_es.json';
+// export { default as pt_pt } from './pt_pt/pt_pt.json'
+// export { default as fr } from './fr/fr.json'
+
+// import en from './en/en.json'
+// import es from './es_es/es_es.json'
+// import pt from './pt_pt/pt_pt.json'
+// import fr from './fr/fr.json'
+
+// export default {
+//   es,
+//   en,
+//   pt,
+//   fr
+// }
---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+    {{ $t("request_your_support") }}
+  </q-page>
+</template>
+
+<script>
+/* eslint-disable quotes */
+/* eslint-disable comma-dangle */
+/* eslint-disable semi */
+export default {
+  name: "PageIndex",
+};
+</script>
